78654eb57cceff481b8fc6ee961e81b2b1e8ca03
Juan C. Laria <juank.laria@gmail.com>
2020-05-24 12:33:26 +0200

    r-base:3.6.3

--- install.R
+++ install.R
@@ -1,28 +1,3 @@
-# Install dependencies
-packages <-
-c("hardhat", 
-"rlang",
-"stats",
-"methods",
-"Rcpp",
-"RcppArmadillo",
-"parsnip",
-#"tibble",
-#"MASS",
-"fda.usc",
-"clues",
-"recipes",
-"testthat",
-#"dplyr",
-#"ggplot2",
-"reshape2",
-"rsample",
-"tune",
-#"devtools",
-"yardstick"
-)
-
-install.packages(packages)
-devtools::install_github("jlaria/glasp")
+devtools::install_github("jlaria/glasp", dependencies = TRUE)
 
 cat("Installation Finished")
